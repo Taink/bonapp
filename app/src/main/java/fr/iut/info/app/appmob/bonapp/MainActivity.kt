fb14d91f2f1bd456df28e1fb0e4689923f82d865
Taink <minemax579@gmail.com>
2021-02-19 20:54:49 +0100

    🚑 Fix FragmentContainerView causing crash at startup
    https://stackoverflow.com/a/59275182/14824745

--- app/src/main/java/fr/iut/info/app/appmob/bonapp/MainActivity.kt
+++ app/src/main/java/fr/iut/info/app/appmob/bonapp/MainActivity.kt
@@ -3,7 +3,7 @@ package fr.iut.info.app.appmob.bonapp
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
@@ -15,12 +15,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
-        val navController = findNavController(R.id.nav_host_fragment)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(setOf(
                 R.id.navigation_home, R.id.navigation_favorites, R.id.navigation_recipes))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
     }
 }
