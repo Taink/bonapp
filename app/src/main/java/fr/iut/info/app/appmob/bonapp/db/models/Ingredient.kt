80193805913748c73f7f606f4acc8799a69b4372
Saibe1111 <sebastien.cuvellier@hotmail.com>
2021-03-02 22:15:44 +0100

    🚧 Add 3 models classes

---
+++ app/src/main/java/fr/iut/info/app/appmob/bonapp/db/models/Ingredient.kt
@@ -0,0 +1,7 @@
+package fr.iut.info.app.appmob.bonapp.db.models
+
+class Ingredient(
+    var name: String,
+    var quantity: Float,
+    var unite: String
+)
---
+++ app/src/main/java/fr/iut/info/app/appmob/bonapp/db/models/Step.kt
@@ -0,0 +1,5 @@
+package fr.iut.info.app.appmob.bonapp.db.models
+
+class Step(
+    var text: String
+)
