d6457582885b8f1ce97bc77ae4ffe4b9da6181e5
Victor <victorbultez2@gmail.com>
2021-03-08 21:08:05 +0100

    🍱 Adding instanciation of the database inside the class :)

--- app/src/main/java/fr/iut/info/app/appmob/bonapp/db/room/AppDatabase.kt
+++ app/src/main/java/fr/iut/info/app/appmob/bonapp/db/room/AppDatabase.kt
@@ -1,9 +1,29 @@
 package fr.iut.info.app.appmob.bonapp.db.room
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
+import java.security.AccessControlContext
 
 @Database(entities = arrayOf(Favorite::class), version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun favoriteDAO(): FavoriteDAO
+
+    companion object{
+        @Volatile
+        private var INSTANCE: AppDatabase?= null
+        fun getDatabase(context: Context):AppDatabase{
+
+            return INSTANCE ?: synchronized(this){
+                val instance= Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java, "local_database"
+                ).build()
+                INSTANCE = instance
+
+                return instance
+            }
+        }
+    }
 }
