e70dc296679178ff00e313cbdf7d9e10491eed69
Taink <minemax579@gmail.com>
2021-03-10 03:16:59 +0100

    🎨 Remove all occurences of "recipe" in ingredient adapter

--- app/src/main/java/fr/iut/info/app/appmob/bonapp/ui/widgets/IngredientRecyclerAdapter.kt
+++ app/src/main/java/fr/iut/info/app/appmob/bonapp/ui/widgets/IngredientRecyclerAdapter.kt
@@ -1,17 +1,11 @@
 package fr.iut.info.app.appmob.bonapp.ui.widgets
 
-import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
-import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import com.bumptech.glide.request.RequestOptions
 import fr.iut.info.app.appmob.bonapp.R
-import fr.iut.info.app.appmob.bonapp.RecipeDetailsActivity
 import fr.iut.info.app.appmob.bonapp.recettes.Ingredient
 
 open class IngredientRecyclerAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
@@ -35,17 +29,17 @@ open class IngredientRecyclerAdapter: RecyclerView.Adapter<RecyclerView.ViewHold
         return items.size
     }
 
-     open fun submitList(recipeList: List<Ingredient>) {
-        items = recipeList
+     open fun submitList(ingredientList: List<Ingredient>) {
+        items = ingredientList
     }
 
     class IngredientViewHolder (
         itemView: View
     ): RecyclerView.ViewHolder(itemView) {
-        val recipeName = itemView.findViewById<TextView>(R.id.ingredient_name)
+        val ingredientName = itemView.findViewById<TextView>(R.id.ingredient_name)
 
         fun bind(ingredient: Ingredient) {
-            recipeName.setText(ingredient.nom)
+            ingredientName.setText(ingredient.nom)
         }
     }
 }
