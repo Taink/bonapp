05b30efcbd5980360f1ebf3e193a7999d5a6b12b
Taink <minemax579@gmail.com>
2021-03-12 15:36:22 +0100

    🩹 Perform requests on database

--- app/src/main/java/fr/iut/info/app/appmob/bonapp/ui/fragments/home/HomeFragment.kt
+++ app/src/main/java/fr/iut/info/app/appmob/bonapp/ui/fragments/home/HomeFragment.kt
@@ -8,95 +8,13 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import fr.iut.info.app.appmob.bonapp.R
+import fr.iut.info.app.appmob.bonapp.controllers.DbController
 import fr.iut.info.app.appmob.bonapp.recettes.RecipePreview
 import fr.iut.info.app.appmob.bonapp.ui.widgets.RecipePreviewRecyclerAdapter
 
 class HomeFragment : Fragment() {
 
     private lateinit var recipeAdapter: RecipePreviewRecyclerAdapter
-    private var randomNbr = 0
-
-    private fun getRandomImage(): String {
-        randomNbr++
-        return "https://picsum.photos/500?random=$randomNbr"
-    }
-
-    private fun createRecipeList(): ArrayList<RecipePreview> {
-        val names = resources.getStringArray(R.array.recipe_names)
-
-        val list = ArrayList<RecipePreview>()
-        list.add(
-            RecipePreview(
-                names.random(),
-                getRandomImage(),
-                false
-            )
-        )
-        list.add(
-            RecipePreview(
-                names.random(),
-                getRandomImage(),
-                true
-            )
-        )
-        list.add(
-            RecipePreview(
-                names.random(),
-                getRandomImage(),
-                false
-            )
-        )
-        list.add(
-            RecipePreview(
-                names.random(),
-                getRandomImage(),
-                false
-            )
-        )
-        list.add(
-            RecipePreview(
-                names.random(),
-                getRandomImage(),
-                true
-            )
-        )
-        list.add(
-            RecipePreview(
-                names.random(),
-                getRandomImage(),
-                true
-            )
-        )
-        list.add(
-            RecipePreview(
-                names.random(),
-                getRandomImage(),
-                true
-            )
-        )
-        list.add(
-            RecipePreview(
-                names.random(),
-                getRandomImage(),
-                false
-            )
-        )
-        list.add(
-            RecipePreview(
-                names.random(),
-                getRandomImage(),
-                false
-            )
-        )
-        list.add(
-            RecipePreview(
-                names.random(),
-                getRandomImage(),
-                false
-            )
-        )
-        return list
-    }
 
     private fun initRecyclerView(root: View) {
         recipeAdapter = RecipePreviewRecyclerAdapter()
@@ -114,8 +32,12 @@ class HomeFragment : Fragment() {
         val root = inflater.inflate(R.layout.fragment_home, container, false)
 
         initRecyclerView(root)
-        val recipeList = createRecipeList()
-        recipeAdapter.submitList(recipeList)
+        val dbController = this.context?.let { DbController(it) }
+
+        dbController?.getRecipesPreviews {
+            val recipeList = it.values.toList()
+            recipeAdapter.submitList(ArrayList(recipeList))
+        }
 
         return root
     }
